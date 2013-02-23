9d75d6c687e829622b04e19df677ed0c7bad2f51
Thad Kerosky <contact@thadk.net>
2013-02-23 14:29:25 -0500

    sett

--- djangoheroku/settings.py
+++ djangoheroku/settings.py
@@ -62,7 +62,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://peaceschool.herokuapp.com/static/'
+STATIC_URL = 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
