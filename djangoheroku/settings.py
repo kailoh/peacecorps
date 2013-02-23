b9a44c0578fca0b55fecfaf96686eb6113e4230c
Thad Kerosky <contact@thadk.net>
2013-02-23 13:46:32 -0500

    settings for static and index view

--- djangoheroku/settings.py
+++ djangoheroku/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -113,7 +113,7 @@ ROOT_URLCONF = 'djangoheroku.urls'
 WSGI_APPLICATION = 'djangoheroku.wsgi.application'
 
 TEMPLATE_DIRS = (
-	"templates"
+	"/app/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
