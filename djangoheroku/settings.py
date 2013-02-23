573057666318f5f5bd94592fe94321ccb7fdd8ed
Thad Kerosky <contact@thadk.net>
2013-02-23 14:01:51 -0500

    add comments module

--- djangoheroku/settings.py
+++ djangoheroku/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/app/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.comments',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'data',
