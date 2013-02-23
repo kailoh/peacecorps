05bb808602e3ca30ad35f453113e22bbea91debd
Thad Kerosky <contact@thadk.net>
2013-02-23 11:34:35 -0500

    further zinnia config incl urls

--- djangoheroku/urls.py
+++ djangoheroku/urls.py
@@ -11,7 +11,22 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^weblog/', include('zinnia.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
 
+#    url(r'^', include('zinnia.urls.capabilities')),
+#    url(r'^search/', include('zinnia.urls.search')),
+#    url(r'^sitemap/', include('zinnia.urls.sitemap')),
+#    url(r'^trackback/', include('zinnia.urls.trackback')),
+    url(r'^blog/tags/', include('zinnia.urls.tags')),
+    url(r'^blog/feeds/', include('zinnia.urls.feeds')),
+#    url(r'^blog/authors/', include('zinnia.urls.authors')),
+#    url(r'^blog/categories/', include('zinnia.urls.categories')),
+#    url(r'^blog/comments/', include('zinnia.urls.comments')),
+#    url(r'^blog/', include('zinnia.urls.entries')),
+#    url(r'^blog/', include('zinnia.urls.archives')),
+#    url(r'^blog/', include('zinnia.urls.shortlink')),
+#    url(r'^blog/', include('zinnia.urls.quick_entry')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
