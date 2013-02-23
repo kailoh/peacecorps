2a20f3f90896929f61d0e84e660ce2aac31a197b
Thad Kerosky <contact@thadk.net>
2013-02-23 14:21:05 -0500

    add comments to requirements

--- djangoheroku/settings.py
+++ djangoheroku/settings.py
@@ -58,14 +58,15 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/app/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://peaceschool.herokuapp.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    'static'
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
