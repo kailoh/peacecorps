8585a692e42de8b20139eb246615f81c7992b097
Thad Kerosky <contact@thadk.net>
2013-02-23 13:51:31 -0500

    settings for static and index view

--- djangoheroku/urls.py
+++ djangoheroku/urls.py
@@ -29,7 +29,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','data.views.home',name='home')
-    # static media¬
+    # static media
     #url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': settings.STATIC_ROOT}),
     #url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
